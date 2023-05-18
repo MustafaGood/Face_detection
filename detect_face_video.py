b83d96e71d26108bc0f3e8ec2f996d77c95198ae
Mustafa Salahuddin <97270548+MustafaGood@users.noreply.github.com>
2023-05-18 18:10:42 +0200

    Update detect_face_video.py

--- detect_face_video.py
+++ detect_face_video.py
@@ -3,11 +3,23 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+# För att använda en videofil som indata
+#cap = cv2.VideoCapture('Mustafavideo.mp4')
+
+
+# Definiera önskad bredd och höjd för den ändrade storleken på videon
+width = 360
+height = 360
+
+
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
 while True:
     ret, frame = cap.read()
+
+       # Ändra storlek på ramen
+    frame = cv2.resize(frame, (width, height))
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -19,10 +31,10 @@ while True:
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
         
-        # Add your name as text
-        cv2.putText(frame, 'Mustafa Salahuddin', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+        # Lägg till ditt namn som text
+        cv2.putText(frame, 'Mustafa ', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
 
-    cv2.imshow('frame', frame)
+    cv2.imshow('Ansiktsigenkänning', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
