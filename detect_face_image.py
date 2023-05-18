7edb2817fb698354b24271b873e0bb1fce9b7332
Mustafa Salahuddin <97270548+MustafaGood@users.noreply.github.com>
2023-05-18 18:11:05 +0200

    Update detect_face_image.py

--- detect_face_image.py
+++ detect_face_image.py
@@ -4,17 +4,31 @@ import cv2
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-img = cv2.imread('Mustafabild.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Läs in bilden
+image = cv2.imread('Mustafas bild.jpg')
+ 
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
 for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    roi_gray = gray[y:y+h, x:x+w]  # Corrected roi dimensions
-    roi_color = img[y:y+h, x:x+w]
+    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = image[y:y+h, x:x+w]
     eyes = eye_cascade.detectMultiScale(roi_gray)
     for (ex, ey, ew, eh) in eyes:
         cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
-cv2.imshow('img', img)
+    # Lägg till ditt namn som text
+    cv2.putText(image, 'Mustafa Salahuddin', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+
+# Visa den annoterade bilden
+cv2.imshow('ansiktsigenkänning', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Lägg till koden för att läsa in 'cric.jpg' och visa den
+img = cv2.imread('Photos/cric.jpg')
+cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
